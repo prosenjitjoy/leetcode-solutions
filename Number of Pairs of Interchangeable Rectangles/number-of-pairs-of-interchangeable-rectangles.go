1a4906a491fdd82211f0464f49720970e03817d2
Prosenjit Majumder Joy <32308244+prosenjitjoy@users.noreply.github.com>
2024-01-11 12:38:15 +0000

    [Medium] [Array,Hash Table,Math,Counting,Number Theory] [284 ms] [26.7 MB]

---
+++ Number of Pairs of Interchangeable Rectangles/number-of-pairs-of-interchangeable-rectangles.go
@@ -0,0 +1,15 @@
+func interchangeableRectangles(rectangles [][]int) int64 {
+    ratio := map[float64]int{}
+    for _, rectangle := range rectangles {
+        ratio[float64(rectangle[0])/float64(rectangle[1])]++
+    }
+
+    var res int64
+    for _, v := range ratio {
+        if v > 0 {
+            res += int64((v * (v-1)) / 2)
+        }
+    }
+
+    return res
+}
