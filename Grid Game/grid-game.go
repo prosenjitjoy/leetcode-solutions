ef7413c47d7920f54ea77ed180c6786c32bd684d
Prosenjit Majumder Joy <32308244+prosenjitjoy@users.noreply.github.com>
2024-01-12 16:37:03 +0000

    [Medium] [Array,Matrix,Prefix Sum] [135 ms] [8.3 MB]

--- Grid Game/grid-game.go
+++ Grid Game/grid-game.go
@@ -1,22 +1,15 @@
 func gridGame(grid [][]int) int64 {
-    n := len(grid[0])
-    prefixSumRow1 := make([]int, n)
-    prefixSumRow2 := make([]int, n)
-    
-    prefixSumRow1[0] = grid[0][0]
-    prefixSumRow2[0] = grid[1][0]
-
-    for i:=1; i<n; i++ {
-        prefixSumRow1[i] = grid[0][i] + prefixSumRow1[i-1]
-        prefixSumRow2[i] = grid[1][i] + prefixSumRow2[i-1]
+    for i:=1; i<len(grid[0]); i++ {
+        grid[0][i] += grid[0][i-1]
+        grid[1][i] += grid[1][i-1]
     }
 
     var res int64 = math.MaxInt64
-    for i:=0; i<n; i++ {
-        top := prefixSumRow1[n-1] - prefixSumRow1[i]
+    for i:=0; i<len(grid[0]); i++ {
+        top := grid[0][len(grid[0])-1] - grid[0][i]
         var bottom int
         if i>0 {
-            bottom = prefixSumRow2[i-1]
+            bottom = grid[1][i-1]
         }
 
         secondRobot := max(top, bottom)
